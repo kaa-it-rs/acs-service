7994c33208a904b5f90cdddf56e69d89bd48bfbb
Andrey Kruglov <kruglov.it@gmail.com>
2022-05-24 03:26:04 +0300

    add opentelemetry

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION := 0.7.0
+VERSION := 1.0.28
 
 check:
 	cargo watch -x check -x test -x run
@@ -22,23 +22,28 @@ test:
 	TEST_LOG=true cargo test | bunyan
 
 debug_up:
-	docker-compose -f docker-compose-debug.yml up -d
+	docker compose -p acs-service-rs -f ./deployment/compose/docker-compose-debug.yml up -d
 
 debug_down:
-	docker-compose -f docker-compose-debug.yml down
+	docker compose -p acs-service-rs -f ./deployment/compose/docker-compose-debug.yml down --remove-orphans
 
 debug_restart: debug_down debug_up
 
 build:
-	docker build -t "opener-service:${VERSION}" ./opener-service
-	docker tag "opener-service:${VERSION}" "akruglov/opener-service:${VERSION}"
-	docker tag "opener-service:${VERSION}" "akruglov/opener-service:latest"
+	docker build -t "acs-service:${VERSION}" ./opener-service
+	docker tag "acs-service:${VERSION}" "akruglov/acs-service:${VERSION}"
+	docker tag "acs-service:${VERSION}" "akruglov/acs-service:latest"
 
 push: build
 	docker login
-	docker image push "akruglov/opener-service:${VERSION}"
-	docker image push "akruglov/opener-service:latest"
+	docker image push "akruglov/acs-service:${VERSION}"
+	docker image push "akruglov/acs-service:latest"
 
 up:
 	docker login
-	docker-compose up -d	
+	docker compose -p acs-service-rs -f ./deployment/compose/docker-compose.yml up -d
+
+down:
+	docker compose -p acs-service-rs -f ./deployment/compose/docker-compose.yml down
+
+restart: down up
--- src/main.rs
+++ src/main.rs
@@ -2,48 +2,55 @@ use std::sync::{atomic::AtomicUsize, Arc};
 
 use actix::*;
 use actix_cors::Cors;
+use actix_web::middleware::Logger;
 use actix_web::{guard, web, App, HttpServer};
 use anyhow::Result;
+use env_logger::Env;
 
-use opener_service::server::OpenerServer;
-use opener_service::{
-    create_schema_with_context, get_count, index_api, index_playground, index_subscriptions,
-    init_db, ws_route,
+use acs_service::server::OpenerServer;
+use acs_service::{
+  create_schema_with_context, get_count, index_api, index_playground, index_subscriptions, init_db,
+  init_tracer, shutdown_tracer, ws_route,
 };
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    env_logger::init();
+  env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
 
-    log::info!("dfsdfsdfdsf");
-    let db = init_db().await?;
+  _ = init_tracer();
 
-    let schema = create_schema_with_context(db.clone());
+  log::info!("dfsdfsdfdsfxx");
+  let db = init_db().await?;
 
-    let openers_count = Arc::new(AtomicUsize::new(0));
+  let schema = create_schema_with_context(db.clone());
 
-    let opener_server = OpenerServer::new(openers_count.clone(), db.clone()).start();
+  let openers_count = Arc::new(AtomicUsize::new(0));
 
-    HttpServer::new(move || {
-        App::new()
-            .wrap(Cors::permissive())
-            .data(openers_count.clone())
-            .data(opener_server.clone())
-            .data(schema.clone())
-            .route("/playground", web::get().to(index_playground))
-            .service(web::resource("/api").guard(guard::Post()).to(index_api))
-            .service(
-                web::resource("/api/ws")
-                    .guard(guard::Get())
-                    .guard(guard::Header("upgrade", "websocket"))
-                    .to(index_subscriptions),
-            )
-            .route("/count/", web::get().to(get_count))
-            .service(web::resource("/ws/").to(ws_route))
-    })
-    .bind("0.0.0.0:8081")?
-    .run()
-    .await?;
+  let opener_server = OpenerServer::new(openers_count.clone(), db.clone()).start();
 
-    Ok(())
+  HttpServer::new(move || {
+    App::new()
+      .wrap(Logger::default())
+      .wrap(Cors::permissive())
+      .app_data(web::Data::new(openers_count.clone()))
+      .app_data(web::Data::new(opener_server.clone()))
+      .app_data(web::Data::new(schema.clone()))
+      .route("/playground", web::get().to(index_playground))
+      .service(web::resource("/api").guard(guard::Post()).to(index_api))
+      .service(
+        web::resource("/api/ws")
+          .guard(guard::Get())
+          .guard(guard::Header("upgrade", "websocket"))
+          .to(index_subscriptions),
+      )
+      .route("/count/", web::get().to(get_count))
+      .service(web::resource("/ws/").to(ws_route))
+  })
+  .bind("0.0.0.0:4000")?
+  .run()
+  .await?;
+
+  shutdown_tracer();
+
+  Ok(())
 }
